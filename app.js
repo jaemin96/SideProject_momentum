3074e081e2f5f3367c19989ae9d9c30cf20381bc
jaemin96 <77824886+jaemin96@users.noreply.github.com>
2021-08-14 00:21:19 +0900

    Object - 복습
    + 기본적인 javascript 문법을 토대로 object 생성부터 add, update 핵심 기능 사용해보며 감 익히기

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
-if(confirm(`Hello`) == true ){
-    alert("me too");
-}
+// people object 생성
+const people = [
+  {
+    name: "김재민",
+    birthDay: "1996-01-28",
+    sex: "male",
+    telephone: "010-4731-9889",
+  },
+];
+
+// 객체와 개체 정보
+console.log(people[0]);
+console.log(people[0].name);
+console.log(people[0].birthDay);
+console.log(people[0].sex);
+console.log(people[0].telephone);
+
+// 개체 정보 변경 (Add, Update)
+// Update!
+people[0].sex = "female";
+// Add!
+people[0].money = "700,000";
+// update object log!
+console.log(people[0]);
+
+// Add subObject
+people.push({
+  name: "케인",
+  birthDay: "1990-11-21",
+  sex: "male",
+  telephone: "010-4432-9952",
+});
+
+console.log(people);
+console.log(people[0]);
+console.log(people[1]);
