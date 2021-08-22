752df61613cbdfa31f49021989f15748a36b3c16
jaemin96 <77824886+jaemin96@users.noreply.github.com>
2021-08-22 17:02:57 +0900

    로그인, 로그아웃 - (3)
    + localStorage 데이터 관리

--- app.js
+++ app.js
@@ -12,26 +12,32 @@ console.log(loginForm, loginInput, logoutBtn, greetMessage);
 // form - input 은 자동적으로 submit이 이루어지면서 새로고침을 기본으로 한다.
 // 이런 기본 기능들을 막기위해 preventDefault() 를 사용한다.
 
-// const newBtn = document.createElement("button");
-// const btnText = document.createTextNode("logout");
+const USER_NAME = "USER_NAME";
 
 const onLogin = (event) => {
   event.preventDefault();
   loginForm.classList.add("hide"); // 로그인 성공 시 폼 숨기기
-  greetMessage.innerText = `Hello, ${loginInput.value}`; // 로그인 성공 시 메세지
-  loginInput.value = ""; // 입력 초기화
+  const name = loginInput.value; //중복 사용되는 입력 값 선언
+  localStorage.setItem(USER_NAME, name); // 로컬스토리지에 아이디 정보 저장
+  greetMessage.innerText = `Hello, ${name}`; // 로그인 성공 시 메세지
   logoutForm.classList.remove("hide"); // 로그인 성공 시 로그아웃 폼 화면에 표시
 };
 
 const onLogout = (event) => {
   event.preventDefault();
+  localStorage.removeItem(USER_NAME); // 로그아웃 시 로그인 정보 삭제
   logoutForm.classList.add("hide"); // 로그아웃 성공 시 폼 숨기기
   loginForm.classList.remove("hide"); // 로그아웃 성공 시 로그인 폼 화면에 표시
 };
 
-// 버튼의 기본 설정은 브라우저에서 설정하고 인풋과 버튼을 감싸는 form 자체에서 이벤트 수행 - submit
-loginForm.addEventListener("submit", onLogin);
-logoutBtn.addEventListener("click", onLogout);
+// 유저 정보가 없는 경우에만 로그인 폼을 보이게 설정
+if (localStorage.getItem(USER_NAME) === null) {
+  logoutForm.classList.add("hide");
+  loginForm.classList.remove("hide");
+  loginForm.addEventListener("submit", onLogin);
+} else {
+  loginForm.classList.add("hide");
+  logoutForm.classList.remove("hide");
+}
 
-// 로그인 폼의 로그인 버튼이 눌렸을 때 로그인 폼을 안보이게 설정하도록
-console.log(loginForm.style);
+logoutBtn.addEventListener("click", onLogout);
