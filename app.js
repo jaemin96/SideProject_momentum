e68c9cb337ab6c7c4b3c10a39d73714ba93368de
jaemin96 <77824886+jaemin96@users.noreply.github.com>
2021-08-23 23:36:48 +0900

    로그인, 로그아웃 - (4) Fin
    - 새로 고침 시 이중 작동 문제 발견
    + 로그인, 로그아웃 코드 refactoring
    + 코드 함수 활용하는 방식으로 수정

--- app.js
+++ app.js
@@ -13,31 +13,54 @@ console.log(loginForm, loginInput, logoutBtn, greetMessage);
 // 이런 기본 기능들을 막기위해 preventDefault() 를 사용한다.
 
 const USER_NAME = "USER_NAME";
+const hidden = "hide";
 
+// exist id!!
+const existId = (e) => {
+  // 아이디 정보가 있을 시, 로그아웃 폼은 표시 로그인 폼은 표시하지 않음
+  logoutForm.classList.remove(hidden);
+  loginForm.classList.add(hidden);
+};
+
+// not exist id!!
+const notExistId = (e) => {
+  // 아이디 정보가 없을 시, 로그인 폼은 표시 로그아웃 폼은 표시하지 않음
+  loginForm.classList.remove(hidden);
+  logoutForm.classList.add(hidden);
+};
+
+// print user name!
+const onPrintName = (name) => {
+  greetMessage.innerText = `Hello, ${name}`;
+};
+
+// login!
 const onLogin = (event) => {
   event.preventDefault();
-  loginForm.classList.add("hide"); // 로그인 성공 시 폼 숨기기
   const name = loginInput.value; //중복 사용되는 입력 값 선언
   localStorage.setItem(USER_NAME, name); // 로컬스토리지에 아이디 정보 저장
-  greetMessage.innerText = `Hello, ${name}`; // 로그인 성공 시 메세지
-  logoutForm.classList.remove("hide"); // 로그인 성공 시 로그아웃 폼 화면에 표시
+  onPrintName(name); // 로그인 성공 시 메세지
+  loginInput.value = "";
+  existId();
 };
 
+// logout!
 const onLogout = (event) => {
   event.preventDefault();
+  notExistId();
   localStorage.removeItem(USER_NAME); // 로그아웃 시 로그인 정보 삭제
-  logoutForm.classList.add("hide"); // 로그아웃 성공 시 폼 숨기기
-  loginForm.classList.remove("hide"); // 로그아웃 성공 시 로그인 폼 화면에 표시
 };
+
+// saved user name
+const SAVED_NAME = localStorage.getItem(USER_NAME);
 
 // 유저 정보가 없는 경우에만 로그인 폼을 보이게 설정
 if (localStorage.getItem(USER_NAME) === null) {
-  logoutForm.classList.add("hide");
-  loginForm.classList.remove("hide");
+  notExistId();
   loginForm.addEventListener("submit", onLogin);
 } else {
-  loginForm.classList.add("hide");
-  logoutForm.classList.remove("hide");
+  existId();
+  onPrintName(SAVED_NAME);
 }
 
 logoutBtn.addEventListener("click", onLogout);
