9a48a23453f919bde2d80d69ecff49502bd2f5fc
jaemin96 <77824886+jaemin96@users.noreply.github.com>
2021-08-27 10:56:07 +0900

    Clock - (3)
    + PM, AM, HH:MM:SS 처리

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const loginForm = document.querySelector("#login");
 const logoutForm = document.querySelector("#logout");
 const logoutBtn = document.querySelector("#logout button");
 const greetMessage = document.querySelector("#logout h1");
+const loginClock = document.querySelector("#logout h2");
 
-console.log(loginForm, loginInput, logoutBtn, greetMessage);
+console.log(loginForm, loginInput, logoutBtn, greetMessage, loginClock);
 
 // 로그안버튼이 눌릴 경우 인풋의 값을 출력!!
 // form - input 은 자동적으로 submit이 이루어지면서 새로고침을 기본으로 한다.
@@ -71,28 +72,38 @@ const setClock = (e) => {
   // 시,분,초 만드는 내가 아는 방법
   // 날짜 객체를 생성해서 객체에서 시,분,초 정보 가져오기
   const recentTime = new Date();
-  const time_hour = recentTime.getHours();
-  const time_min = recentTime.getMinutes();
-  const time_sec = recentTime.getSeconds();
+  let time_hour = recentTime.getHours();
+  let time_min = recentTime.getMinutes();
+  let time_sec = recentTime.getSeconds();
 
-  // 구닥다리 방법, 노가다
-  // 시,분,초 한자리수 0붙이는건 추후 조사
-  // time_hour < 10
-  //   ? console.log(`0${time_hour}:${time_min}:${time_sec}`)
-  //   : console.log(`${time_hour}:${time_min}:${time_sec}`);
+  // 시간이 한 자릿수 일 때
+  if (time_hour < 10) {
+    time_hour = `0${time_hour}`;
+  }
 
-  // (time_hour < 10) & (time_min < 10)
-  //   ? console.log(`0${time_hour}:0${time_min}:${time_sec}`)
-  //   : console.log(`${time_hour}:${time_min}:${time_sec}`);
+  // 분이 한 자릿수 일 때
+  if (time_min < 10) {
+    time_min = `0${time_min}`;
+  }
 
-  // time_sec < 10
-  //   ? console.log(`${time_hour}:${time_min}:0${time_sec}`)
-  //   : console.log(`${time_hour}:${time_min}:${time_sec}`);
+  // 초가 한 자릿수 일 때
+  if (time_sec < 10) {
+    time_sec = `0${time_sec}`;
+  }
 
-  console.log(`${time_hour}:${time_min}:${time_sec}`);
+  // AM과 PM으로 시간 구분하기
+  if (time_hour > 12) {
+    loginClock.innerText = `${time_hour - 12}:${time_min}:${time_sec}[PM]`;
+  } else {
+    loginClock.innerText = `${time_hour}:${time_min}:${time_sec}[AM]`;
+  }
+
+  // 이 코드가 정상 작동하는 이유는 0을 붙힌 시, 분, 초가 유지되는 것이 아닌 1초마다 함수가 재실행되어 get 메소드를 반복하기 떄문에
+  // 10이 되는 순간 if문은 작동하지 않고 새로운 시, 분, 초가 할당되기 때문에 010이 아닌 10으로 정상적으로 작동하게 된다.
 };
 
-setClock();
-
 // 시계 만들기 - setInterval
+// 1000ms = 1s , 1초마다 setClock 함수를 실행한다.
+// 리엑트에서는 useEffect를 사용해서 렌더링을 제어할 수 있다.
+// 지금은 새로고침하면 0.5~1초 뒤에 카운트가 시작되는 문제가 발생
 setInterval(setClock, 1000);
